7d136bcd69cc669e3aba09422136858de920bd93
Denis Mazalov <denismazalov09@gmail.com>
2025-12-19 22:38:48 +0300

    v 0.1.1

--- Project.py
+++ Project.py
@@ -2,6 +2,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 def getPressedKeys(event):
+    '''Получение нажатых кнопок'''
     global pressedKeys
     pressedKeys.add(event.keysym)
     if event.keysym == 'a' or event.keysym == 'd':
@@ -9,42 +10,76 @@ def getPressedKeys(event):
             startMove()
 
 def getReleasedKeys(event):
-    global pressedKeys, isMoving
+    '''Получение отжатых кнопок'''
+    global pressedKeys, isMoving, directionX
+    if event.keysym == 'space':
+        if not isJumping:
+            startJump()
+    else:
+        directionX = 0
     pressedKeys.discard(event.keysym)
 
 def move():
-    global targetX
+    '''Передвижение на a/d'''
+    global directionX
     if isMoving:
         currentX = canvas.coords(mario)[0]
         if 'a' in pressedKeys:
-            targetX = currentX - 10
             canvas.itemconfig(mario, image=marioPhotoFlipped)
+            directionX = -1
         elif 'd' in pressedKeys:
-            targetX = currentX + 10
             canvas.itemconfig(mario, image=marioPhoto)
-        updateMove(targetX, currentX)
+            directionX = 1
+        updateMove(directionX, currentX)
         window.after(16, move)
-
+def jump():
+    '''Прыжок'''
+    global targetY
+    global velocity
+    if isJumping:
+        currentY = canvas.coords(mario)[1]
+        updateJump(currentY)
+        window.after(16, jump)
+        
 def startMove():
     global isMoving       
     isMoving = True
     move()
 
-def updateMove(targetX, currentX):
-    global isMoving
-    step = 0
-    distance = targetX - currentX
-    if abs(distance) > 1:
-        step = distance * 0.8
-        currentX += step
-    else:
-        isMoving = False
-        if abs(step) > abs(distance):
-            currentX = targetX
-    canvas.coords(mario, currentX, canvas.coords(mario)[1])
+def startJump():
+    global isJumping
+    global velocity
+    isJumping = True
+    velocity = -30
+    jump()
 
+def updateMove(directionX, currentX):
+    '''Анимация передвижения'''
+    global isMoving
+    currentX += directionX * 8 
+    if directionX == 0:
+        isMoving = False
+    canvas.coords(mario, currentX, canvas.coords(mario)[1])
+    
+def updateJump(currentY):
+    '''Анимация прыжка'''
+    global isJumping
+    global velocity
+    velocity = velocity + gravity * 0.016
+    currentY += velocity
+    if currentY + 55 > canvas.coords(ground)[1] and canvas.coords(mario)[0] < canvas.coords(ground)[2]:
+        currentY = canvas.coords(ground)[1] - 55
+        velocity = 0
+        isJumping = False
+    canvas.coords(mario, canvas.coords(mario)[0], currentY)
+
+isJumping = False
 isMoving = False
-targetX = 0
+isOnGround = True
+velocity = 0
+gravity = 200
+directionX = 0
+targetY = 0
 pressedKeys = set()
 
 #Создание окна
@@ -57,11 +92,12 @@ icon = PhotoImage(file='mario_icon.png')
 window.iconphoto(False, icon)
 canvas = Canvas(window, bg='lightblue', width=screenWidth, height=screenHeight)
 canvas.pack(anchor='center')
+ground = canvas.create_line(0, screenHeight // 2 + 200, screenWidth - 100, screenHeight // 2 + 200, fill='black', width=8) 
 
 #Создание персонажа
 marioPhoto = ImageTk.PhotoImage(Image.open('mario_sprite.png').resize((100, 100)))
 marioPhotoFlipped = ImageTk.PhotoImage(Image.open('mario_sprite.png').resize((100, 100)).transpose(Image.FLIP_LEFT_RIGHT))
-mario = canvas.create_image(screenWidth // 2, screenHeight // 2, image=marioPhoto)
+mario = canvas.create_image(screenWidth // 2, screenHeight // 2 + 145, image=marioPhoto)
 
 #Управление
 window.bind('<KeyPress>', getPressedKeys)
